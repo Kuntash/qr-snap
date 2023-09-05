7b8513856b963e8221603674c92dc42a3a6843bb
Kunga Tashi <kuntashtashi11@gmail.com>
2023-09-06 03:02:09 +0530

    set up with shadcn/ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@main/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { inter } from "@main/fonts"
+import "@main/styles/globals.css"
+import type { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
